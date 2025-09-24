bf5f549c64dab0b4027b202dd1a3ba2cee80eb4b
asherKaltura <asher.desta@kaltura.com>
2025-09-24 21:04:15 +0300

    modify api test

--- src/main/java/utils/DriverManager.java
+++ src/main/java/utils/DriverManager.java
@@ -2,8 +2,11 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverManager {
@@ -12,24 +15,46 @@ public class DriverManager {
     public static WebDriver getDriver() {
         if (driver == null) {
             String browser = ConfigReader.get("browser").toLowerCase();
+            boolean headless = Boolean.parseBoolean(ConfigReader.get("headless", "false"));
+
             switch (browser) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    if (headless) {
+                        chromeOptions.addArguments("--headless=new");
+                        chromeOptions.addArguments("--no-sandbox");
+                        chromeOptions.addArguments("--disable-dev-shm-usage");
+                    }
+                    driver = new ChromeDriver(chromeOptions);
                     break;
+
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    if (headless) {
+                        firefoxOptions.addArguments("--headless");
+                    }
+                    driver = new FirefoxDriver(firefoxOptions);
                     break;
+
                 case "edge":
                     WebDriverManager.edgedriver().setup();
-                    driver = new EdgeDriver();
+                    EdgeOptions edgeOptions = new EdgeOptions();
+                    if (headless) {
+                        edgeOptions.addArguments("--headless=new");
+                    }
+                    driver = new EdgeDriver(edgeOptions);
                     break;
+
                 default:
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
             }
-            driver.manage().window().maximize();
+
+            if (!headless) {
+                driver.manage().window().maximize();
+            }
         }
         return driver;
     }
