08d20b65800ddafb064332ef401dc896a91d6ea2
asherKaltura <asher.desta@kaltura.com>
2025-09-27 21:10:54 +0300

    add suiteXmlFile to pom

--- src/main/java/utils/ConfigReader.java
+++ src/main/java/utils/ConfigReader.java
@@ -1,20 +1,23 @@
 package utils;
 
-import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReader {
     private static Properties prop = new Properties();
 
     static {
-        try {
-            FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
-            prop.load(fis);
+        try (InputStream input = ConfigReader.class.getClassLoader()
+                .getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.err.println("Sorry, unable to find config.properties");
+            } else {
+                prop.load(input);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
     public static String get(String key) {
         if (System.getProperty(key)==null){
             return prop.getProperty(key);
